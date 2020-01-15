a7edf9683e8c0641ee0dd6aac5a5ad413b0265c8
Damon Chhun <damonchhun7@gmail.com>
2020-01-14 21:51:35 -0800

    robofriends

---
+++ robofriends/src/CardArray.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from './Card';
+
+const CardArray = ({ robots }) => {
+    return(
+        <div>
+            {
+                robots.map((users, i) => {
+                    return(<Card key = {i} 
+                    id ={robots[i].id}
+                    name = {robots[i].name}
+                    email ={robots[i].email} 
+                    username = {robots[i].username} 
+                    />)
+                })
+            }
+        </div>
+)
+}
+export default CardArray
