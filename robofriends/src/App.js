fd1ad0b2d43f9e72c307b3ffdc815bc494906fc1
Damon Chhun <damonchhun7@gmail.com>
2020-01-16 14:26:45 -0800

    first iteration of search box and title

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import CardArray from './CardArray'
 import {robots} from './robots'
+import Searchbox from './Searchbox'
 
 const App = () => {
     return (
         <div className = 'tc'>
+            <h1>RoboFriends!</h1>
+            <Searchbox />
             <CardArray robots = { robots } />
         </div>
     )
